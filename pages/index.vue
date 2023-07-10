4bf7ccd8741fb6bafa3fe7d49287950eda43e1c9
Diseive <104061331+Diseive@users.noreply.github.com>
2023-07-10 05:56:23 +0000

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
+    <!-- ナビゲーションバー -->
+    <nav>
+      <ul>
+        <li><a href="#">財務会計アプリ</a></li>
+      </ul>
+    </nav>
+
     <!-- アプリのUIコンポーネント -->
-    <h1 style="color:#00F">財務会計アプリ</h1>
     <p style="color:#0FF">残高 </p>
     <p>{{ balance }}</p>
-    <input type="number" v-model="transactionAmount" placeholder="金額を入力してください">
+    <input type="number" v-model="transactionAmount" placeholder="金額を入力してください" />
     <button @click="addTransaction">入金</button>
     <button @click="withdrawTransaction">引き出し</button>
 
@@ -18,43 +24,62 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      balance: 0,
-      transactionAmount: 0,
-      transactionHistory: []
-    };
-  },
-  methods: {
-    addTransaction() {
-      // 入金処理
-      const transaction = {
-        id: Date.now(),
-        description: "入金",
-        amount: this.transactionAmount
-      };
-      this.transactionHistory.push(transaction);
-      this.balance += this.transactionAmount;
-      this.transactionAmount = 0;
-    },
-    withdrawTransaction() {
-      // 引き出し処理
-      if (this.transactionAmount > this.balance) {
-        alert("残高が不足しています");
-        return;
-      }
+<script setup>
+import { ref } from 'vue';
 
-      const transaction = {
-        id: Date.now(),
-        description: "引き出し",
-        amount: -this.transactionAmount // 引き出しは負の値として表現する
-      };
-      this.transactionHistory.push(transaction);
-      this.balance -= this.transactionAmount;
-      this.transactionAmount = 0;
-    }
+const balance = ref(0);
+const transactionAmount = ref(0);
+const transactionHistory = ref([]);
+
+const addTransaction = () => {
+  // 入金処理
+  const transaction = {
+    id: Date.now(),
+    description: '入金',
+    amount: transactionAmount.value,
+  };
+  transactionHistory.value.push(transaction);
+  balance.value += transactionAmount.value;
+  transactionAmount.value = 0;
+};
+
+const withdrawTransaction = () => {
+  // 引き出し処理
+  if (transactionAmount.value > balance.value) {
+    alert('残高が不足しています');
+    return;
   }
+
+  const transaction = {
+    id: Date.now(),
+    description: '引き出し',
+    amount: -transactionAmount.value, // 引き出しは負の値として表現する
+  };
+  transactionHistory.value.push(transaction);
+  balance.value -= transactionAmount.value;
+  transactionAmount.value = 0;
 };
 </script>
+
+<style>
+nav {
+  background-color: #333;
+  color: #fff;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  padding: 0;
+}
+
+nav ul li {
+  margin: 10px;
+}
+
+nav ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
